fe9cd09224b05574d50b8b0ed87d4c6b2d071688
R-o-y <royal845426589@gmail.com>
2016-05-26 20:16:14 +0800

    基本上完成了sign up的部分 只剩下最后一个问题暂时没有解决，就是； 邮箱是唯一identification，所以邮箱不能重复，如果用户用于注册的邮箱已经被注册或者正在被注册，要令其重新选择email address

--- Orbital_django/home/static/assets/js/scripts.js
+++ Orbital_django/home/static/assets/js/scripts.js
@@ -1,7 +1,4 @@
-/*
-children() 返回所有下一级匹配的子元素 immediate children
-find() 返回所有匹配的子元素 descendants
-*/
+var email_address;
 
 jQuery(document).ready(function() {
 
@@ -36,7 +33,7 @@ jQuery(document).ready(function() {
         });
     });
 
-    // next step
+    // first step's next step
     $('.registration-form .btn-next').click(function() {
     	var parentRegistrationForm = $(this).parents('.registration-form');
     	var nextStep = false;
@@ -55,6 +52,8 @@ jQuery(document).ready(function() {
         	});
 
         	if(nextStep) {
+                email_address = parentRegistrationForm.find('input[type="email"]').val();
+
         		parentRegistrationForm.children('fieldset').fadeOut(400, function() {
     	    		parentRegistrationForm.next().children("fieldset").fadeIn();
     	    	});
@@ -62,13 +61,48 @@ jQuery(document).ready(function() {
     	});
 
     });
-    
-    // previous step
-    $('.registration-form .btn-previous').click(function() {
+
+    // second step
+    $('#sign_me_up_button').on('click', function(e) {
         var parentRegistrationForm = $(this).parents('.registration-form');
-    	parentRegistrationForm.children('fieldset').fadeOut(400, function() {
-            parentRegistrationForm.prev().children("fieldset").fadeIn();
+        parentRegistrationForm.find('input[type="text"], input[type="password"], input[type="email"]').each(function() {
+            if( $(this).val() == "" ) {
+                $(this).addClass('input-error');
+            }
+            else {
+                $(this).removeClass('input-error');
+                $.ajax({
+                    type: "POST",
+                    url: "/handle_sign_up",
+                    data: {
+                        csrfmiddlewaretoken: getCookie('csrftoken'),
+                        verification_code: $("input[name='verification_code']").val(),
+                        email_address: email_address,
+                    },
+                    success: function (data) {
+                        if (data == "wrong") {
+                            document.getElementById("message2").innerHTML="<font color='red'>verification code is incorrect</font>";
+                        }
+                        else {
+                            window.location.href = "/";  // return to home page
+                        }
+                    },
+                })
+            }
         });
+    });
+
+    // when leave sign_up_page, send information to the server to delete the information for this signing up
+    $(window).unload(function(){
+        $.ajax({
+            type: "POST",
+            url: "/handle_sign_up",
+            data: {
+                csrfmiddlewaretoken: getCookie('csrftoken'),
+                leave: "yes",
+                email_address: email_address,
+            },
+        })
     });
 
     // clear
@@ -77,24 +111,41 @@ jQuery(document).ready(function() {
     });
 
     // password validate
-    $(".registration-form[target='for_submit_refresh_3']").find("input[name='password_confirm']").keyup(function () {
+    $(".registration-form[target='for_submit_refresh']").find("input[name='password_confirm']").keyup(function () {
         var pw1 = document.getElementsByName("password")[0].value;
         var pw2 = document.getElementsByName("password_confirm")[0].value;
         if(pw2.length < 6){
             document.getElementById("message").innerHTML="<font color='red'>Password must be longer than or equal to 6 digts.</font>";
-            document.getElementById("sign_me_up_button").disabled = true;
+            document.getElementById("next_button").disabled = true;
         }
         else if(! /^(?=.*[a-z])[a-z0-9]+/ig.test(pw2)){
             document.getElementById("message").innerHTML="<font color='red'>Password may contain only letters or numbers.</font>";
-            document.getElementById("sign_me_up_button").disabled = true;
+            document.getElementById("next_button").disabled = true;
         }
         else if(pw1 == pw2) {
             document.getElementById("message").innerHTML="<font color='green'>Password confirmed.</font>";
-            document.getElementById("sign_me_up_button").disabled = false;
-        } else {
+            document.getElementById("next_button").disabled = false;
+        } 
+        else {
             document.getElementById("message").innerHTML="<font color='red'>Passwords mismatch.</font>";
-            document.getElementById("sign_me_up_button").disabled = true;
+            document.getElementById("next_button").disabled = true;
         }
     });
     
 });
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
--- Orbital_django/home/views.py
+++ Orbital_django/home/views.py
@@ -24,7 +24,7 @@ def display_sign_up_page(request):
 def handle_log_in(request):
     input_email_address = request.POST['email_address']
     input_password = request.POST['password']
-    user = authenticate(input_username = input_email_address, input_password = input_password) # need to rewrite authenticate because this default authenticate use the User in AUTHENTICATION AND AUTHORIZATION rathen than in HOME
+    user = authenticate(input_username=input_email_address, input_password=input_password)
     if user is not None:
         if user.is_active:
             login(request, user)
@@ -35,25 +35,59 @@ def handle_log_in(request):
         return HttpResponse("<h1>email address or password is wrong</h1>")
 
 
-new_user = models.User()
+# temp_user_information_dic is a python dictionary
+# the key is the email address
+# the value is a 2-item list
+# the first item is the user whose signing-up email is the key
+# the second item is the verification code for this user
+temp_user_information_dic = {}
 
 
 def handle_sign_up(request):
+
     post_result_dic = request.POST
+    global temp_user_information_dic
 
     if post_result_dic.__contains__("username"):
+        new_user = models.User()
         new_user.set_username(post_result_dic["username"])
+    
         if post_result_dic.__contains__("email_address"):
             new_user.set_email_address(post_result_dic["email_address"])
 
             # send verification code by email
             verification_code = random.randint(0, 99999)
             verification_code_str = str(verification_code).zfill(5)
-            verification_email = EmailMessage("verification code", verification_code_str, "obitoonepatchman@gmail.com", [post_result_dic["email_address"]])
-            verification_email.send(fail_silently = False)
+            correct_verification_code_str = verification_code_str
+            verification_email = EmailMessage("verification code",
+                                              verification_code_str,
+                                              "obitoonepatchman@gmail.com",
+                                              [post_result_dic["email_address"]])
+            verification_email.send(fail_silently=False)
 
-    if post_result_dic.__contains__("password"):
-        new_user.set_password(post_result_dic["password_confirm"])
-        new_user.save()
+            if post_result_dic.__contains__("password"):
+                new_user.set_password(post_result_dic["password_confirm"])
 
-    return HttpResponse()
+                # when a user finish the first step of signing up,
+                # store a new user and his/her verification code in temp_user_information_dic
+                temp_user_information_dic[post_result_dic["email_address"]] = [new_user, correct_verification_code_str]
+                return HttpResponse()
+
+    elif post_result_dic.__contains__("verification_code"):
+        email_address = post_result_dic["email_address"]
+        this_user_information = temp_user_information_dic[email_address]
+        if post_result_dic["verification_code"] == this_user_information[1]:
+            this_user_information[0].save()
+
+            # when a user finish signing up and leave the sign_up_page, 
+            # delete his/her temp information stored in temp_user_information_dic
+            del temp_user_information_dic[email_address]
+            return HttpResponse()
+        else:
+            return HttpResponse("wrong")
+
+    # when a user leave the sign_up_page, 
+    # delete his/her temp information stored in temp_user_information_dic
+    elif post_result_dic.__contains__("leave"):
+        email_address = post_result_dic["email_address"]
+        del temp_user_information_dic[email_address]
