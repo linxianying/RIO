7924046da44f1b5ca9958d8100a97a3ac74a350b
R-o-y <royal845426589@gmail.com>
2016-06-25 22:50:07 +0800

    没别的，就是删掉刚才那个多余的pdf

--- Orbital_django/user_dashboard/models.py
+++ Orbital_django/user_dashboard/models.py
@@ -5,9 +5,7 @@ from file_viewer.models import Document
 
 
 # class Album(models.Model):
-#     name = models.CharField(max_length=128)
+#     name = models.CharField(max_length=256)
+#     introduction = models.TextField()
 #     owner = models.ForeignKey(User)
-#     members = models.ManyToManyField(User, related_name="joined_album_set", blank=True)
-#     applicants = models.ManyToManyField(User, related_name="applied_album_set", blank=True)
-#     publicity = models.BooleanField()
 #     documents = models.ManyToManyField(Document, related_name="belong_album_set", blank=True)
